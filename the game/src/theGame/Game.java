24dafbf666355afb5568fd2c19dbd1d14ca9375d
Oken735 <oken735@yahoo.com>
2013-12-06 14:31:24 -0500

    made the message actually work
    oken735

--- the game/src/theGame/Game.java
+++ the game/src/theGame/Game.java
@@ -1,12 +1,17 @@
 package theGame;
 
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Game {
 	public static void main(String[] args) {
+		JFrame frame = new JFrame("sweetnesss");
 		JPanel panel = new JPanel();
 		JLabel label1 = new JLabel("this is gona be sweet");
 		panel.add(label1);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
 	}
 }
