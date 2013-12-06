43f288d03fda4bcfdfa8b9a5ed46d3bd529caa7c
Oken735 <oken735@yahoo.com>
2013-12-06 14:10:21 -0500

    start message

---
+++ the game/src/theGame/Game.java
@@ -0,0 +1,12 @@
+package theGame;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Game {
+	public static void main(String[] args) {
+		JPanel panel = new JPanel();
+		JLabel label1 = new JLabel("this is gona be sweet");
+		panel.add(label1);
+	}
+}
